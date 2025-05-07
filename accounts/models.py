32775aaf9a844c5258ddbeb21dfa6ff7891f269a
allankalema4 <allankalema4@gmail.com>
2025-05-07 10:46:03 +0300

    accounts models added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add any additional fields here
+    typing_speed_goal = models.IntegerField(default=40)  # Words per minute goal
+    
+    def __str__(self):
+        return self.username
