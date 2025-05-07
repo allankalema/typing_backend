574b5f116927ae127b333e3b8d25991ea35c227b
allankalema4 <allankalema4@gmail.com>
2025-05-07 10:54:36 +0300

    made migrations for the  database and created a superuser

--- typings/views.py
+++ typings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import TextPassage, TypingSession
+from .serializers import TextPassageSerializer, TypingSessionSerializer, UserProgressSerializer
+from accounts.models import CustomUser
 
-# Create your views here.
+class TextPassageList(generics.ListAPIView):
+    queryset = TextPassage.objects.all()
+    serializer_class = TextPassageSerializer
+    permission_classes = [permissions.AllowAny]
+
+class TypingSessionCreate(generics.CreateAPIView):
+    queryset = TypingSession.objects.all()
+    serializer_class = TypingSessionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(user=self.request.user)
+        else:
+            serializer.save()
+
+class UserProgressView(generics.RetrieveAPIView):
+    serializer_class = UserProgressSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user
